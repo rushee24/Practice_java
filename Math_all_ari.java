a3ddd71e2a0a960d8b0796c2d0e3f8cf9ac865d8
Rushikesh khillare <80709809+rushee24@users.noreply.github.com>
2023-08-28 18:43:38 +0530

    Create Math_all_ari.java

---
+++ Math_all_ari.java
@@ -0,0 +1,21 @@
+public class Math_all_ari {
+
+public static void main(String args[]) {
+  int a = 50;
+  int b = 10;
+
+  int sum = a+b;
+  int product = a * b;
+  int difference = a - b;
+  int division = a / b;  
+  int remainder = a % a;
+
+  System.out.println("Sum= " + sum);
+  System.out.println("Product= " + product);
+  System.out.println("Difference= " + difference);
+  System.out.println("Division= " + division);
+  System.out.println("Remainder= " + remainder);
+
+}
+
+};
