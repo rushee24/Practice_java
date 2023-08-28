dcf15aee4370bf021018e4598416b94d865fb304
Rushikesh khillare <80709809+rushee24@users.noreply.github.com>
2023-08-28 18:39:45 +0530

    Create Swap_the_numbe.java

---
+++ Swap_the_numbe.java
@@ -0,0 +1,15 @@
+class Swap_the_number{
+ public static void main(String[] args) {
+   int a = 40;
+   int b = 30;
+
+  int temp = a;
+  a = b;
+  b = a;
+System.out.println("a="+a + " b" +b);
+System.out.println("After swap:");
+System.out.println("num1= " + a);
+System.out.println("num2= " + b);
+
+}
+};
