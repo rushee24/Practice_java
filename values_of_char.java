4f0b398e4055c9d6d7a32c622dac8fd9278baf69
Rushikesh khillare <80709809+rushee24@users.noreply.github.com>
2023-08-28 18:49:06 +0530

    Create values_of_char.java

---
+++ values_of_char.java
@@ -0,0 +1,11 @@
+public class values_of_char {
+    public static void main(String[] args) {
+        char character = 'r';
+
+        int values = (int) character;
+
+        System.out.println("The Value is: " + values);
+
+    }
+
+};
