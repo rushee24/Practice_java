cd5591fc4063c3968cc28128d813ec3663cc570b
Rushikesh khillare <80709809+rushee24@users.noreply.github.com>
2023-08-31 10:58:54 +0530

    Create con_d_r.java

---
+++ con_d_r.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class jds1 {
+    public static void main(String[] args) {
+        int n=72;
+        Scanner sc=new Scanner(System.in);
+        int doller=sc.nextInt();
+        int rupay=n*doller;
+        System.out.println(rupay);
+    }
+}
